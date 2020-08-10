ce5d144b1bd50d5e8a81e51604273467ade191b3
Niclas Alexandersson <alniniclas@gmail.com>
2020-08-10 15:42:08 +0200

    Added script for computing summary statistics.

--- scripts/summary_statistics.py
+++ scripts/summary_statistics.py
@@ -4,14 +4,23 @@ import math
 from collections import namedtuple
 
 
-SummaryStatistics = namedtuple('SummaryStatistics', ('measurements', 'mean', 'variance', 'stddev', 'stderr', 'cv'))
+SummaryStatistics = namedtuple(
+  'SummaryStatistics',
+  ('measurements', 'mean', 'median', 'min', 'max', 'quartiles', 'variance', 'stddev', 'stderr', 'cv')
+)
 
 
 def summary_statistics(values):
   measurements = len(values)
   mean = statistics.mean(values)
+  median = statistics.median(values)
+  min_val = min(values)
+  max_val = max(values)
+  quantiles = statistics.quantiles(values)
   variance = statistics.variance(values)
   stddev = statistics.stdev(values)
   stderr = stddev / math.sqrt(measurements)
   cv = stddev / mean
-  return SummaryStatistics(measurements, mean, variance, stddev, stderr, cv)
+  return SummaryStatistics(
+    measurements, mean, median, min_val, max_val, quantiles, variance, stddev, stderr, cv
+  )
